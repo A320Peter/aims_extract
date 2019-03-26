b4ebe76c84fb92bcdc3cba34fc01bdb4b3a9cd83
Jon Hurst <jon@hursts.org.uk>
2019-03-26 12:39:10 +0000

    Updated version to 0.2.post1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="aims_extract",
-    version="0.2",
+    version="0.2.post1",
     packages=["aims"],
     python_requires='>=3.6',
     install_requires=['Beautifulsoup4', 'requests', 'python-dateutil'],
